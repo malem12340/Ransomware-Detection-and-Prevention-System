27607b0715134104415ee9dc3c8c35ace85cd73e
Malemnganba Ningthoujam <malem.fw.wb@gmail.com>
2026-09-11 21:55:05 +0530

    Fix database environment configuration

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-from urllib.parse import urlparse
 
 
 class Config:
@@ -9,19 +8,9 @@ class Config:
         "Ransomware_Project_2026"
     )
 
-    DB_URL = os.environ.get("MYSQL_PUBLIC_URL")
-
-    if DB_URL:
-        db_url = urlparse(DB_URL)
-
-        DB_HOST = db_url.hostname
-        DB_PORT = db_url.port
-        DB_USER = db_url.username
-        DB_PASSWORD = db_url.password
-        DB_NAME = db_url.path.lstrip("/")
-    else:
-        DB_HOST = "localhost"
-        DB_PORT = 3306
-        DB_USER = "root"
-        DB_PASSWORD = ""
-        DB_NAME = "ransomware2_db"
+    # Database configuration
+    DB_HOST = os.environ.get("DBHOST", "localhost")
+    DB_PORT = int(os.environ.get("DBPORT", 3306))
+    DB_USER = os.environ.get("DBUSER", "root")
+    DB_PASSWORD = os.environ.get("DBPASSWORD", "")
+    DB_NAME = os.environ.get("DBNAME", "ransomware2_db")
