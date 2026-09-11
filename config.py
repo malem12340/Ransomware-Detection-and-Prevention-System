8321d1b0f7d9b57dd31b7529e85d50badeb05ab1
Malemnganba Ningthoujam <malem.fw.wb@gmail.com>
2026-09-11 18:10:09 +0530

    Fix Railway public MySQL connection

--- config.py
+++ config.py
@@ -1,14 +1,27 @@
 import os
+from urllib.parse import urlparse
 
 
 class Config:
+
     SECRET_KEY = os.environ.get(
         "SECRET_KEY",
         "Ransomware_Project_2026"
     )
 
-    DB_HOST = os.environ.get("DB_HOST")
-    DB_USER = os.environ.get("DB_USER")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD")
-    DB_NAME = os.environ.get("DB_NAME")
-    DB_PORT = int(os.environ.get("DB_PORT", "3306"))
+    DB_URL = os.environ.get("MYSQL_PUBLIC_URL")
+
+    if DB_URL:
+        db_url = urlparse(DB_URL)
+
+        DB_HOST = db_url.hostname
+        DB_PORT = db_url.port
+        DB_USER = db_url.username
+        DB_PASSWORD = db_url.password
+        DB_NAME = db_url.path.lstrip("/")
+    else:
+        DB_HOST = "localhost"
+        DB_PORT = 3306
+        DB_USER = "root"
+        DB_PASSWORD = ""
+        DB_NAME = "ransomware2_db"
