6955a7671873bb9c31e6b06a500a5b37c1572daf
Malemnganba Ningthoujam <malem.fw.wb@gmail.com>
2026-09-11 22:52:32 +0530

    Use public Railway MySQL connection

--- config.py
+++ config.py
@@ -8,9 +8,8 @@ class Config:
         "Ransomware_Project_2026"
     )
 
-    # Database configuration
     DB_HOST = os.environ.get("DBHOST", "localhost")
-    DB_PORT = int(os.environ.get("DBPORT", 3306))
+    DB_PORT = int(os.environ.get("DBPORT", "3306"))
     DB_USER = os.environ.get("DBUSER", "root")
     DB_PASSWORD = os.environ.get("DBPASSWORD", "")
     DB_NAME = os.environ.get("DBNAME", "ransomware2_db")
